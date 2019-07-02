a2157afacfec68091b18832c6d5fe5e33758839d
Sonsoles López Pernas <sonsoleslp@hotmail.com>
2019-07-02 13:18:57 +0200

    Demonstrator

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var bodyParser = require('body-parser')
 
 
 server.listen(8080, function() {
@@ -11,16 +12,16 @@ server.listen(8080, function() {
 
 io.on('connection', function(socket) {
 	console.log('Un cliente se ha conectado');
-   /* setInterval(function(){
-    	io.sockets.emit('messages', {avg: Math.random()});
-    },1000)*/
 });
 
 app.use(express.static('public'));
 
-
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.text())
+ 
+// parse application/json
+app.use(bodyParser.json())
 app.post("/avg",function(req,res){
-	console.log("avg")
-	io.sockets.emit('messages', {avg: Math.random()});
+	io.sockets.emit('messages', {avg: req.body});
 	res.send(200)
 })
